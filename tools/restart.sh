1b035abddd050c661d69df5748097523d078d0a7
tangfu <abctfq2008@gmail.com>
2012-10-12 15:44:59 +0800

    Update tools/restart.sh

--- tools/restart.sh
+++ tools/restart.sh
@@ -6,5 +6,5 @@ BASE_PATH=$(dirname `which $0`)
 cd ${BASE_PATH}
 
 sleep 1
-./${PROJ_NAME} -conf=${PROJ_NAME}.cfg -daemon=true -logbufsecs=0 -v=1
+./${PROJ_NAME} -conf=${PROJ_NAME}.conf -daemon=true -logbufsecs=0 -v=1
 
