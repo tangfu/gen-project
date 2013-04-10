b7508c012067bd13e6ddec0f3ae3983afa8b4809
tangfu <abctfq2008@gmail.com>
2013-04-10 20:41:28 +0800

    添加log_dirs目录的指定

--- tools/restart.sh
+++ tools/restart.sh
@@ -8,7 +8,7 @@ cd ${BASE_PATH}
 sleep 1
 #这里是方便测试环境所做的参数配置
 #运营环境需要做适当调整，例如可能会关掉logbufsecs=0以及v=1
-../build/src/${PROJ_NAME} -conf=../${PROJ_NAME}.conf -daemon=true -logbufsecs=0 -stderrthreshold=3 -v=1
+../build/src/${PROJ_NAME} -conf=../${PROJ_NAME}.conf -log_dir=../logs -daemon=true -logbufsecs=0 -stderrthreshold=3 -v=1
 
-#../bin/${PROJ_NAME} -conf=../conf/${PROJ_NAME}.conf -daemon=true -stderrthreshold=3
+#../bin/${PROJ_NAME} -conf=../conf/${PROJ_NAME}.conf -log_dir=../logs -daemon=true -stderrthreshold=3
 
